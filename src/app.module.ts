a24b3ec5f85ee5f4c2fb4f9e39b5b950e28ec0b8
TranVanTienDat <ckim32993@gmail.com>
2024-10-26 21:16:58 +0700

    Update erorr

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@/config/config.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from 'config/config.module';
 
 @Module({
   imports: [
