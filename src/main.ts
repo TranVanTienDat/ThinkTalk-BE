c712b5945bc57bbb1e84059995bad20acf7633d7
TranVanTienDat <ckim32993@gmail.com>
2024-12-08 17:01:41 +0700

    config enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -8,17 +8,24 @@ const SWAGGER_TITLE = 'Chat app API';
 const SWAGGER_DESCRIPTION = 'API used for Chat app management';
 const SWAGGER_PREFIX = '/docs';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api/v1/');
-
+function createSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle(SWAGGER_TITLE)
     .setDescription(SWAGGER_DESCRIPTION)
     .addBearerAuth()
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(SWAGGER_PREFIX, app, documentFactory);
+  const document = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_PREFIX, app, document);
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1/');
+
+  if (!process.env.SWAGGER_ENABLE || process.env.SWAGGER_ENABLE === '1') {
+    createSwagger(app);
+  }
+  app.enableCors({ origin: '*' });
 
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
