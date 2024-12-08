67097965e968321829deaa5c53dff618d0fca230
TranVanTienDat <ckim32993@gmail.com>
2024-12-08 19:13:15 +0700

    add swagger-ui  cdn

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,16 @@ function createSwagger(app: INestApplication) {
     .addBearerAuth()
     .build();
   const document = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(SWAGGER_PREFIX, app, document);
+  SwaggerModule.setup(SWAGGER_PREFIX, app, document, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 }
 
 async function bootstrap() {
