e9b70b5ba81c2fd653ca28aeb87dbc9f20059e8b
TranVanTienDat <ckim32993@gmail.com>
2025-06-23 10:50:15 +0700

    Test tsconfig

--- dist/config/app.config.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: (() => {
-    port: string;
-}) & import("@nestjs/config").ConfigFactoryKeyHost<{
-    port: string;
-}>;
-export default _default;
--- dist/entities/attachment.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from './message.entity';
-export declare class Attachment {
-    id: number;
-    file_url: string;
-    file_type: string;
-    uploaded_at: Date;
-    message: Message;
-}
